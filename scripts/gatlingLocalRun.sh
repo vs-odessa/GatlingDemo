8b411de47e4ab68df6fbf6d90918a72993f3d814
Vladimir Stepanov <vladimir.stepanov@lohika.com>
2017-03-23 10:49:45 +0200

    Gatling script optimization for local run & customizing reports directory to simulation name.

--- scripts/gatlingLocalRun.sh
+++ scripts/gatlingLocalRun.sh
@@ -3,17 +3,11 @@ set -x #echo on
 ##################################################################################################################
 #Gatling scale out/cluster run script:
 #Before running this script some assumptions are made:
-#1) Public keys were exchange inorder to ssh with no password promot (ssh-copy-id on all remotes)
-#2) Check  read/write permissions on all folders declared in this script.
-#3) Gatling installation (GATLING_HOME variable) is the same on all hosts
-#4) Assuming all hosts has the same user name (if not change in script)
+#Check  read/write permissions on all folders declared in this script.
 ##################################################################################################################
 
-#Assuming same user name for all hosts
-USER_NAME='root'
-
-#Remote hosts list
-HOSTS=()
+#Change to your simulation class name
+#SIMULATION_NAME='computerdatabase.BasicSimulation' # Defined in Jenkins job
 
 #Assuming all Gatling installation in same path (with write permissions)
 GATLING_HOME=$WORKSPACE #Is equal to /home/jenkins/workspace/$JOB_NAME
@@ -21,35 +15,28 @@ GATLING_SIMULATIONS_DIR=$GATLING_HOME/user-files/simulations
 GATLING_RUNNER=$GATLING_HOME/bin/gatling.sh
 chmod +x $GATLING_HOME/bin/*
 
-#Change to your simulation class name
-#SIMULATION_NAME='computerdatabase.BasicSimulation' # Defined in Jenkins job
-
 #No need to change this
 GATLING_REPORT_DIR=$GATLING_HOME/results/
-GATHER_REPORTS_DIR=$GATLING_HOME/mygatling-reports/
+GATHER_REPORTS_DIR=$GATLING_HOME/$SIMULATION_NAME-report/
 
-echo "Starting Gatling cluster run for simulation: $SIMULATION_NAME"
-
-echo "Cleaning previous runs from localhost"
+echo "Cleaning previous runs from localhost..."
 rm -rf $GATHER_REPORTS_DIR
 mkdir $GATHER_REPORTS_DIR
 rm -rf $GATLING_REPORT_DIR
 
-echo "Running simulation on localhost"
+echo "Running simulation on localhost..."
 $GATLING_RUNNER -nr -s $SIMULATION_NAME
 
-sleep 10
+sleep 5
 
-#echo "Gathering result file from localhost"
-# /root/gatling-reports/  /root/gatling-reports/report
+echo "Gathering result file from localhost..."
 ls -t $GATLING_REPORT_DIR | head -n 1 | xargs -I {} mv ${GATLING_REPORT_DIR}{} ${GATLING_REPORT_DIR}report
 cp ${GATLING_REPORT_DIR}report/simulation.log $GATHER_REPORTS_DIR
 
 
 mv $GATHER_REPORTS_DIR $GATLING_REPORT_DIR
 echo "Generate report for local run..."
-$GATLING_RUNNER -ro mygatling-reports 
-#$GATLING_RUNNER -ro report
+$GATLING_RUNNER -ro $SIMULATION_NAME-report 
 
 #using macOSX
 #open ${GATLING_REPORT_DIR}reports/index.html
